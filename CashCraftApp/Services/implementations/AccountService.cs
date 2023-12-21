4b973d56c8a6046141f44f575f6e94ddce878074
Ayannuga Gbenga <65336545+gbbest15@users.noreply.github.com>
2023-12-22 00:12:28 +0100

    account created done

--- CashCraftApp/Services/implementations/AccountService.cs
+++ CashCraftApp/Services/implementations/AccountService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
 using CashCraftApp.DAL;
 using CashCraftApp.Model;
 using Microsoft.EntityFrameworkCore;
@@ -25,7 +27,17 @@ namespace CashCraftApp.Services.implementations
             if (_db.Accounts.Any(x => x.Email == account.Email)) throw new ApplicationException("A user with this email exists");
             if (!Pin.Equals(ConfirmPin)) throw new ApplicationException("Pins do not match.");
 
-            
+            byte[] pinHash, pinSalt;
+
+            createHashingPassword(Pin, out pinHash, out pinSalt);
+
+            account.PinHash = pinHash;
+            account.PinSalt = pinSalt;
+
+            _db.Accounts.Add(account);
+            _db.SaveChanges();
+
+            return account;
         }
 
         public void Delete(int Id)
@@ -52,6 +64,19 @@ namespace CashCraftApp.Services.implementations
         {
             throw new NotImplementedException();
         }
+
+
+        private static void createHashingPassword(string pin, out byte[] pinHash, out byte[] pinSalt)
+        {
+            if (string.IsNullOrEmpty(pin)) throw new ArgumentNullException("Pin is null or Empty");
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                pinSalt = hmac.Key;
+                pinHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(pin));
+            }
+        }
+
+
     }
 }
 
